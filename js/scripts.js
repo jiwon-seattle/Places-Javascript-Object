9d475a11564825e4456328a50f2441637a799e6e
Jiwon Han <42082640+jiwon-seattle@users.noreply.github.com>
2020-01-27 10:46:00 -0800

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,20 @@
+//Business Logic for Place -------------
+function Place(location, landmarks, time, notes) {
+  this.location = location;
+  this.landmarks = landmarks;
+  this.time = time;
+  this.notes = notes
+}
+
+Place.prototype.details = function() {
+  return "Location: " + this.location + "<br>Landmarks: <br>" + this.landmarks.join("<br>") + "<br>Best time to visit: " + this.time + "<br>Notes: " + this.notes.join(", ");
+}
+
+var seattle = new Place("Washington State", ["Space Needle", "Pike Place Market", "Capitol Hill"], "Summer", ["Coffee", "Rain", "IT", "Liberal"])
+
+var korea = new Place("East Asia", ["Seoul", "Busan"], "Spring", ["K-pop", "Night life", "Food"])
+
+var newYork = new Place("USA", ["Empire State Building", "Central Park", "The Statue of Liberty"], "Fall", ["fashion", "Broadway"])
 
 // User Interface
 $(document).ready(function(){
@@ -16,20 +33,3 @@ $(document).ready(function(){
 })
 
 
-//Business Logic for Place -------------
-function Place(location, landmarks, time, notes) {
-  this.location = location;
-  this.landmarks = landmarks;
-  this.time = time;
-  this.notes = notes
-}
-
-Place.prototype.details = function() {
-  return "Location: " + this.location + "<br>Landmarks: <br>" + this.landmarks.join("<br>") + "<br>Best time to visit: " + this.time + "<br>Notes: " + this.notes.join(", ");
-}
-
-var seattle = new Place("Washington State", ["Space Needle", "Pike Place Market", "Capitol Hill"], "Summer", ["Coffee", "Rain", "IT", "Liberal"])
-
-var korea = new Place("East Asia", ["Seoul", "Busan"], "Spring", ["K-pop", "Night life", "Food"])
-
-var newYork = new Place("USA", ["Empire State Building", "Central Park", "The Statue of Liberty"], "Fall", ["fashion", "Broadway"])
